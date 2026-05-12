9b5c70c764f702b70fa3a03fae6278ecc15245e1
showallproject <showallproject@gmail.com>
2026-05-12 16:47:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+// fullname =prompt ("what is your name ?");
+// console.log("hello"+ fullname)
+//ocument.getElementById("hed").innerHTML = "hello" + " " + fullname;
+function myfunction(){
+    let name=document.getElementById("name").value;
+    document.getElementById("hed").innerHTML = "hello" + " " + name;
+}
+let name = document.getElementById("name");
+let button = document.getElementById("btn");
+
+name.addEventListener("keydown", function (event) {
+
+    if (event.key === "Enter") {
+        myfunction();  
+    }
+});
